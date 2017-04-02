c9b3c09f11909d1688ab85c1d250f33f876eeb6c
yago <yrodrigez@gmail.com>
2017-04-02 16:21:13 +0200

    display options to progress bar

--- ProgressBar.js
+++ ProgressBar.js
@@ -1,27 +1,40 @@
 var ProgressBar = function (id) {
-  this.id = ((id === undefined) ? 'accis-file-load-progress-bar' : id);
-  this.cssId = '#'+this.id;
-  this.skin = '<div id="'+ this.id +'" class="finished"></div>';
-  this.element = document.getElementById(this.id);
+    this.id = ((id === undefined) ? 'accis-file-load-progress-bar' : id);
+    this.cssId = '#' + this.id;
+    this.skin = '<div id="' + this.id + '" class="finished"></div>';
+    this.element = document.getElementById(this.id);
 
-  var mElement = this.element;
-  this.updateProgress = function (progress) {
-      if(progress === 0) mElement.style.marginTop = '0';
-      else {
-          mElement.style.width = progress + '%';
-          mElement.style.marginTop = '.5em';
-      }
-  };
+    var mElement = this.element;
+    this.updateProgress = function (progress) {
+        mElement.style.width = progress + '%';
+    };
 
-  this.isActive = function () {
-      return mElement.className.match(/(\\s*|^)active(\\s*|$)/);
-  };
+    this.isActive = function () {
+        return mElement.className.match(/(\\s*|^)active(\\s*|$)/);
+    };
 
-  this.activate = function () {
-    mElement.className = mElement.className.replace(/(\\s*|^)inactive(\\s*|$)/, 'active');
-  };
+    this.activate = function () {
+        mElement.style.marginTop = '.5em';
+        mElement.className = mElement.className.replace(/(\\s*|^)inactive(\\s*|$)/, 'active');
+    };
 
-  this.deactivate =  function () {
-      mElement.className = mElement.className.replace(/(\\s*|^)active(\\s*|$)/, 'inactive');
-  };
+    this.deactivate = function () {
+        mElement.className = mElement.className.replace(/(\\s*|^)active(\\s*|$)/, 'inactive');
+    };
+
+    this.hide = function () {
+        mElement.style.display = 'none';
+    };
+
+    this.show = function () {
+        mElement.style.display = 'block';
+    };
+
+    this.toggle = function () {
+        if (mElement.style.display === 'none') {
+            mElement.style.display = 'block';
+        } else {
+            mElement.style.display = 'none';
+        }
+    }
 };
