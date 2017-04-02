bd8f6efb32a17817f023f586bf45eb99d48f396f
yago <yrodrigez@gmail.com>
2017-04-02 16:13:32 +0200

    changed CSV functions

--- CSVFunctions.js
+++ CSVFunctions.js
@@ -1,19 +1,17 @@
 function CSVStringToObject(csvString) {
 
     var lines = csvString.trim().split('\n');
-
-    console.log(lines); //TODO DELETE THIS LINE
     var header = lines[0].split(',');
     this.header = header;
     var body = [];
     lines.forEach(function (line) {
         var lineArray = line.split(',');
         if ((lineArray.length > 0) && (lineArray.length !== header.length)) throw 'CSV malformed';
-        body.push(lineArray);
+        else {
+            body.push(lineArray);
+        }
     });
     this.body = body;
-
-    console.log(body);
 
     return this;
 }
